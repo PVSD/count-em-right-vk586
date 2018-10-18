b612f80a6deb1f94d5e7224b7f2ff874c970ca04
Valentina Kononenko <valentina@kononenko.net>
2018-10-17 20:51:23 -0400

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        String input, sp[];
+        boolean loop = true;
+        while (loop) {
+            System.out.print("Please type a sentence. ");
+            input = sc.nextLine();
+            if(input.equals("EXITZ")) {
+                loop = false;
+            }
+            input = input + "Z";
+            input = input.toUpperCase();
+
+            sp = input.split("S\\s*A");
+
+            System.out.println("There are " + (sp.length - 1) + " occurrences.");
+            System.out.println("");
+
+        }
     }
 }
